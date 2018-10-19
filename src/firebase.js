370abe40a633659505bfdb264a3a12b1687ff2fe
gizodynamics <44292065+gizodynamics@users.noreply.github.com>
2018-10-19 16:22:55 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import "firebase/storage";
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDBjiZfuXZD9rezV9fDO4DPxxgnSAbzY8s",
-  authDomain: "react-slack-clone-59674.firebaseapp.com",
-  databaseURL: "https://react-slack-clone-59674.firebaseio.com",
-  projectId: "react-slack-clone-59674",
-  storageBucket: "react-slack-clone-59674.appspot.com",
-  messagingSenderId: "450668846019"
+  
 };
 firebase.initializeApp(config);
 
